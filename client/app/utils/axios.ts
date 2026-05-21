4292dc84ca7963e5d8bcdd531d83c5619ad799c4
mongonsors <mongonsors@yahoo.com>
2026-05-21 08:26:38 +0800

    fix api base url

--- client/app/utils/axios.ts
+++ client/app/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://food-ahv2.onrender.com",
+  baseURL: "https://foooddd.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
