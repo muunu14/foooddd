7a5eae1b21dbdd085aeba9db29167fcb6ffe5bdf
mongonsors <mongonsors@yahoo.com>
2026-05-21 08:37:38 +0800

    fix api base url88

--- client/app/utils/axios.ts
+++ client/app/utils/axios.ts
@@ -9,9 +9,12 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken");
+
   if (token) {
+    config.headers = config.headers || {};
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
